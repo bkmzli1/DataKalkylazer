7aa8dd36465be1f795f419f2330a69a9f1757817
bkmzli1 <43925220+bkmzli1@users.noreply.github.com>
2019-04-08 15:47:05 +0500

    Java_test

--- src/ru/bkmz/Main.java
+++ src/ru/bkmz/Main.java
@@ -1,8 +1,45 @@
 package ru.bkmz;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.SocketHandler;
+
 public class Main {
+    static Lock lock = new ReentrantLock();
 
     public static void main(String[] args) {
-	// write your code here
+        new Thread1().start();
+        new Thread2().start();
+    }
+
+    static class Thread1 extends Thread {
+        @Override
+        public void run() {
+            lock.lock();
+            System.out.println(getName() + "stat");
+            try {
+                sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(getName() + "stop");
+            lock.unlock();
+            System.out.println(getName() + "unlock");
+        }
+    }
+
+    static class Thread2 extends Thread {
+        @Override
+        public void run() {
+            System.out.println(getName() + "stat");
+            while (true) {
+                if (lock.tryLock()) {
+                    System.out.println(getName() + "Working");
+                    break;
+                }else {
+                    System.out.println(getName() + "waiting");
+                }
+            }
+        }
     }
 }
---
+++ src/ru/bkmz/Main2.java
@@ -0,0 +1,7 @@
+package ru.bkmz;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+    }
+}
