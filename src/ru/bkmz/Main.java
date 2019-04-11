8b7e534bf5abfc094db39b5ccd3dc4cb637da113
bkmzli1 <43925220+bkmzli1@users.noreply.github.com>
2019-04-11 17:59:33 +0500

    Java_test

--- src/ru/bkmz/Main.java
+++ src/ru/bkmz/Main.java
@@ -1,45 +1,10 @@
 package ru.bkmz;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.logging.SocketHandler;
+import javax.swing.*;
 
 public class Main {
-    static Lock lock = new ReentrantLock();
-
     public static void main(String[] args) {
-        new Thread1().start();
-        new Thread2().start();
-    }
-
-    static class Thread1 extends Thread {
-        @Override
-        public void run() {
-            lock.lock();
-            System.out.println(getName() + "stat");
-            try {
-                sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(getName() + "stop");
-            lock.unlock();
-            System.out.println(getName() + "unlock");
-        }
-    }
-
-    static class Thread2 extends Thread {
-        @Override
-        public void run() {
-            System.out.println(getName() + "stat");
-            while (true) {
-                if (lock.tryLock()) {
-                    System.out.println(getName() + "Working");
-                    break;
-                }else {
-                    System.out.println(getName() + "waiting");
-                }
-            }
-        }
+        GUI gui = new GUI();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
--- src/ru/bkmz/Main2.java
+++
@@ -1,7 +0,0 @@
-package ru.bkmz;
-
-public class Main2 {
-    public static void main(String[] args) {
-
-    }
-}
